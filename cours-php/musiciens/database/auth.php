5f131723e508c04c6c286cabf4dd86295aa90dc5
seasone <contact.seasone@gmail.com>
2024-03-11 11:56:51 +0100

    cours php/mysql

---
+++ cours-php/musiciens/database/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+ define("DSN" ,"mysql:host=localhost;charset=utf8;dbname=db_musiciens");
+ define("USER","root");
+ define("PWD","root");
+ # si vous utilisez MAMP avec OS, alors : define("PWD","root");
--- cours-php/musiciens/index.php
+++ cours-php/musiciens/index.php
@@ -1,8 +1,29 @@
+<?php
+# Etape 1 Connexion
+require "database/auth.php";
+
+# Tentativ de connexion avec PDO
+try{
+    $dbh = new PDO(DSN, USER, PWD);
+}catch(PDOException $e){
+    exit("<h1>Erreur connexion serveur</h1>");
+}
+# Etape 2 requête brute dansla table musiciens
+$query =  $dbh->query("SELECT * FROM musiciens");
+
+// Etape 3 transformer la requete en Type de donnée exploitable pour l'affichage
+$tab =  $query->fetchAll(PDO::FETCH_ASSOC);
+
+// test
+//echo "<pre>" ;
+//var_dump($tab);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>page des musiciens</title>
     <link rel="stylesheet" href="./style.css">
 </head>
@@ -10,7 +31,9 @@
     <h1>Artistes guitaristes</h1>
     <main class="main-section">
         <nav class="menu">
-
+            <?php for($i=0;$i< count($tab);$i++): ?>
+                <p><?= $tab[$i]['nom'] ?></p>
+            <?php endfor ?>
         </nav>
         <section class="detail">
 
