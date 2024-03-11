90be8cbccf045c52ebc614e5dd8b111d9d4f2736
seasone <contact.seasone@gmail.com>
2024-03-11 11:24:53 +0100

    cours php/mysql

---
+++ cours-php/musiciens/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page des musiciens</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1>Artistes guitaristes</h1>
+    <main class="main-section">
+        <nav class="menu">
+
+        </nav>
+        <section class="detail">
+
+        </section>
+    </main>
+</body>
+</html>
+
+<!-- 
+    Objectif
+    1. placer dans le menu les noms des artistes
+    2. dans la section detail , toutes les données relatives à la sélection faite dans le menu
+
+ -->
